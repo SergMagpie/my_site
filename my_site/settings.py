b4ff87fa423c1bdec08c2b3482a4bdb0f66e6443
SergMagpie <77747152+SergMagpie@users.noreply.github.com>
2021-07-02 18:12:43 +0300

    up

--- my_site/settings.py
+++ my_site/settings.py
@@ -159,7 +159,7 @@ DEFAULT_FILE_STORAGE = 'my_site.storage_backends.MediaStorage'
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django_ses.SESBackend'
-AWS_SES_REGION_NAME = config('REGION-NAME')  
+AWS_SES_REGION_NAME = 'eu-west-3'  
 # (ex: us-east-2)
 # # (ex: email.us-east-2.amazonaws.com)
-AWS_SES_REGION_ENDPOINT = config('REGION-ENDPOINT')
+AWS_SES_REGION_ENDPOINT = 'email.eu-west-3.amazonaws.com'
